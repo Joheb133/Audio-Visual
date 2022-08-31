a5df7fc1fa236224ff2f6a70d03900a6a4cfbfc3
zoheb598 <joheb133@gmail.com>
2022-08-31 03:59:36 +0100

    Added ability to search music

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+//audio init
 const audioCtx = new AudioContext();
 const gainNode = audioCtx.createGain();
 
@@ -8,50 +9,75 @@ const bufferLength = analyser.frequencyBinCount;
 const dataArray = new Uint8Array(bufferLength);
 analyser.getByteTimeDomainData(dataArray);
 
-// Connect the source to be analysed
-loadSound("aMGmXzpy84g");
+// Music search
+const searchBtn = document.getElementById("search-button");
+const searchBar = document.getElementById("search-bar");
+let searchVal;
+
+searchBtn.addEventListener("click", () => {
+  searchVal = searchBar.value;
+  if (searchVal.match("youtube.com/watch\\?v=")) {
+    let videoCode = searchVal.slice(searchVal.indexOf("youtube.com/watch?v=") + "youtube.com/watch?v=".length, searchVal.length);
+    if (videoCode.length > 1) {
+      console.log("Searching");
+      loadSound(videoCode)
+    } else {
+      console.log("Invalid link");
+    };
+  } else {
+    console.log("Enter a youtube link");
+  };
+});
+//Create + connect the source to be analysed
 function loadSound(link) {
   const request = new XMLHttpRequest();
   request.open("GET", `http://localhost:8000/stream/${link}`, true);
   request.responseType = "arraybuffer";
 
-
   request.onload = function () {
-      const Data = request.response;
-      console.log(Data)
-      process(Data);
+    const Data = request.response;
+    process(Data);
+    console.log("Playing audio")
   };
 
   request.send();
 }
-
+let source;
 function process(Data) {
-  const source = audioCtx.createBufferSource(); // Create Sound Source
+  if(typeof source === "object"){
+    source.stop()
+  }
+  source = audioCtx.createBufferSource(); // Create Sound Source
   audioCtx.decodeAudioData(Data, function (buffer) {
-      source.buffer = buffer;
-      source.connect(gainNode)
-      gainNode.connect(audioCtx.destination);
-      source.start(audioCtx.currentTime);
+    source.buffer = buffer;
+    source.connect(gainNode)
+    gainNode.connect(audioCtx.destination);
+    source.connect(analyser);
+    source.start(audioCtx.currentTime);
   });
-  source.connect(analyser);
 }
 
 /* audio controls */
 //play/pause
-const play = document.getElementById("play");
-const pause = document.getElementById("pause");
+const playBtn = document.getElementById("play");
+let play = false;
 
-play.addEventListener("click", ()=>{
-  audioCtx.resume();
-});
-pause.addEventListener("click", ()=>{
-  audioCtx.suspend();
+playBtn.addEventListener("click", () => {
+  if (play) {
+    audioCtx.resume();
+    play = false;
+    playBtn.innerText = "Pause"
+  } else if (!play) {
+    audioCtx.suspend();
+    play = true;
+    playBtn.innerText = "Play"
+  }
 });
 
 //volume
 const volumeControl = document.getElementById("volume");
-volumeControl.addEventListener('input', ()=> {
-	gainNode.gain.value = volumeControl.value;
+volumeControl.addEventListener('input', () => {
+  gainNode.gain.value = volumeControl.value;
 }, false);
 
 
