2803db9985f0628dc431459b39e8aefa46eb13a7
zoheb598 <joheb133@gmail.com>
2022-09-03 21:21:30 +0100

    FIX: Play/pause bug
    Play pause would break when search & spamming play button

--- public/utils/gui.js
+++ public/utils/gui.js
@@ -4,19 +4,29 @@ import { loadSound, source } from "./load-sound.js";
 //play/pause
 const playBtn = document.getElementById("play")
 const playPathEl = document.getElementById("play-path")
-let play = false;
+let playing = false;
+audioCtx.suspend();
 
 playBtn.addEventListener("click", () => {
-  if (play) {
-    audioCtx.resume(); //import
-    play = false;
-    playPathEl.setAttribute("d", "M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z");//pause
-  } else if (!play) {
-    audioCtx.suspend();
-    play = true;
-    playPathEl.setAttribute("d", "M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z");//play
+  if (playing) {
+    pause();
+  } else if (!playing) {
+    play();
   }
 });
+
+function play() {
+  console.log("play")
+  audioCtx.resume(); //import
+  playPathEl.setAttribute("d", "M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z");//pause
+  playing = true;
+}
+function pause() {
+  console.log("pause")
+  audioCtx.suspend();
+  playPathEl.setAttribute("d", "M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z");//play
+  playing = false;
+}
 //progress bar
 const progressBar = document.getElementById("progress");
 const songDurationEl = document.getElementById("song-duration");
@@ -31,11 +41,11 @@ let searchVal;
 
 searchBtn.addEventListener("click", () => {
   if (sourceCount >= 1) {
-    playPathEl.setAttribute("d", "M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z");//play
     sourceCount--;
+    pause();
     source.stop();
   };
-  
+
   searchVal = searchBar.value;
   if (searchVal.match("youtube.com/watch\\?v=")) {
     let videoCode = searchVal.slice(searchVal.indexOf("youtube.com/watch?v=") + "youtube.com/watch?v=".length, searchVal.length);
@@ -43,9 +53,8 @@ searchBtn.addEventListener("click", () => {
       console.log("Searching");
       //need an import
       loadSound(videoCode, function () {
-        audioCtx.resume()
+        play()
         console.log("Playing audio");
-        playPathEl.setAttribute("d", "M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z");//pause
         songDurationEl.innerText = new Date(1000 * source.buffer.duration).toISOString().substring(14, 19)
         sourceCount++;
       });
